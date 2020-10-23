c4669e58b47179ab6d6e6243b68b8e67acc75609
Kevzor <73276550+kevroma@users.noreply.github.com>
2020-10-23 17:02:33 +0800

    Update ipconvert.ps1

--- ipconvert.ps1
+++ ipconvert.ps1
@@ -11,6 +11,6 @@
     ForEach($IP in Get-Content $EC2IP)
     {
     $InstanceID = aws ec2 describe-instances --filter Name=private-ip-address,Values=$IP --query 'Reservations[].Instances[].InstanceId' --output text | Out-String
-    Edit-EC2InstanceAttribute -InstanceId $InstanceID.Trim() -Group sg-03c2d658b011a403c
+    Edit-EC2InstanceAttribute -InstanceId $InstanceID.Trim() -Group <security_groupid> #put your security group id
     }
 }
